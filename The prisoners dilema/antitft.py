5d7adcb4980b3ba2dc0d57235dcda85da29e5660
HolaSoyLuki <lucvermeulenc@gmail.com>
2025-10-02 09:19:45 +0200

    Update antitft.py

--- The prisoners dilema/antitft.py
+++ The prisoners dilema/antitft.py
@@ -1,1 +1,10 @@
-
+def calcs(iP, hP, points):
+    play = None
+    if iP == None:
+        play = 1
+    else:
+        if iP == 1:
+            play = 0
+        elif iP == 0:
+            play = 1
+    return play
